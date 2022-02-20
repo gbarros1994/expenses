cf87fe17fe35119be9d91dfa8f9197a29bf66ee5
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-02-20 20:30:21 -0300

    cards

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,25 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Versão Inicial'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      ),
     );
   }
 }
