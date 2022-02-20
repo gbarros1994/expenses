4f0df6ff45a574551cc1efda613692cec04bf9f3
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-02-20 20:43:51 -0300

    objects in visual elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_v1/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -13,7 +14,21 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
+  
+  final _transactions = [
+    Transaction(
+      id: 't1', 
+      title: 'Novo tênis', 
+      value: 310.76, 
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Conta de Luz', 
+      value: 211.76, 
+      date: DateTime.now()
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +46,13 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de Transações'),
-          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
