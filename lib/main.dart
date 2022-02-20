895a8c3054da57b8073289a53ae140019ab27476
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-02-20 20:58:37 -0300

    colors

--- lib/main.dart
+++ lib/main.dart
@@ -72,9 +72,21 @@ class MyHomePage extends StatelessWidget {
                      ),
                    ),
                    Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
                      children: [
-                       Text(tr.title),
-                       Text(tr.date.toString())
+                       Text(
+                         tr.title,
+                         style: TextStyle(
+                           fontSize: 16,
+                           fontWeight: FontWeight.bold
+                         ),
+                         ),
+                       Text(
+                         tr.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey
+                        ),
+                         )
                      ],
                    )
                  ]),
