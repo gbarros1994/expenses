dc2b0f68e4cc735a8dcf93f39770c5161f73ec20
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-02-28 23:10:36 -0300

    theme color

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -27,7 +27,7 @@ class TransactionList extends StatelessWidget {
                 ),
                 decoration: BoxDecoration(
                   border: Border.all(
-                    color: Colors.purple,
+                    color: Theme.of(context).primaryColor,
                     width: 2,
                   )
                 ),
@@ -37,7 +37,7 @@ class TransactionList extends StatelessWidget {
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
-                    color: Colors.purple
+                    color: Theme.of(context).primaryColor
                   ),
                 ),
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber
+      ),
     );
   }
 }
