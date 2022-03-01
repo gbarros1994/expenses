d59aca44c565e66af287b2790830448738a5b685
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-02-28 23:14:38 -0300

    fix theme

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -27,7 +27,7 @@ class TransactionList extends StatelessWidget {
                 ),
                 decoration: BoxDecoration(
                   border: Border.all(
-                    color: Theme.of(context).primaryColor,
+                    color: Theme.of(context).colorScheme.primary,
                     width: 2,
                   )
                 ),
@@ -37,7 +37,7 @@ class TransactionList extends StatelessWidget {
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
-                    color: Theme.of(context).primaryColor
+                    color: Theme.of(context).colorScheme.primary
                   ),
                 ),
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,30 @@ class ExpensesApp extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
+    final ThemeData tema = ThemeData();
+ 
     return MaterialApp(
       home: MyHomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.amber
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
