5c4363e14e944d6b1de34f22122d27f24e016a87
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-03-05 20:07:16 -0300

    create component chart

---
+++ lib/components/chart.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
