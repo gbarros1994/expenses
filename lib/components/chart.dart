fe644b251b2faccfecc074ff0f77dbfd43f36493
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-03-05 20:37:56 -0300

    create component chart bar

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -27,9 +27,6 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay)[0]);
-      print(totalSum);
-
       return {
         'day': DateFormat.E().format(weekDay)[0], 
         'value': totalSum
@@ -44,7 +41,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactions.map((tr) {
+          return Text('${tr['day']}: ${tr['value']}');
+        }).toList(),
       ),
     );
   }
