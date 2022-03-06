33ac1f44756b576836d36c2497b419ecb7e557be
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-03-05 23:30:50 -0300

    chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_v1/components/chart_bar.dart';
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
@@ -42,7 +43,7 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactions.map((tr) {
-          return Text('${tr['day']}: ${tr['value']}');
+          return ChartBar(tr['day'].toString(), 2, 0);
         }).toList(),
       ),
     );
