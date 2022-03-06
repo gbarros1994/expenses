cabbbaccb98cdd4ac2e0e3f88c0d279b5ddfacc7
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-03-06 16:29:32 -0300

    chart

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -21,7 +21,30 @@ class ChartBar extends StatelessWidget {
         Container(
           height: 60,
           width: 10,
-          child: null,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
         SizedBox(height: 5),
         Text(label),
