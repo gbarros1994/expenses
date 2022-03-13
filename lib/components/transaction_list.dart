e2aedc28528a0cbe32e4680013a7489d7bd019a2
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-03-13 20:03:24 -0300

    refactor list

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -34,48 +34,21 @@ class TransactionList extends StatelessWidget {
         itemCount: transactions.length,
         itemBuilder: (ctx, index) {
           final tr = transactions[index];
-          return Card(
-            child: Row(children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).colorScheme.primary,
-                    width: 2,
-                  )
-                ),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'R\$ ' +tr.value.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Theme.of(context).colorScheme.primary
-                  ),
-                ),
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: Padding(
+                padding: const EdgeInsets.all(6),
+                child: FittedBox(child: Text('R\$${tr.value}')),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    tr.title,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold
-                    ),
-                    ),
-                  Text(
-                  DateFormat('d/MM/y').format(tr.date),
-                  style: TextStyle(
-                    color: Colors.grey
-                  ),
-                    )
-                  ],
-                )
-              ]),
+            ),
+            title: Text(
+              tr.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            subtitle: Text(
+              DateFormat('d MMM y').format(tr.date)
+            ),
           );
         },
       ),
